c98cf3cee6cc209b4f8e68fb7e44fdcb4712377b
Andrés Gomez <agomez@wizzie.io>
2018-11-13 13:36:29 +0100

    Fix init script

--- stack-start.sh
+++ stack-start.sh
@@ -2,16 +2,16 @@
 
 # Start Zookeeper
 
-/kafka_2.11-1.0.0/bin/zookeeper-server-start.sh -daemon /kafka_2.11-1.0.0/config/zookeeper.properties
+/kafka_2.11-1.1.0/bin/zookeeper-server-start.sh -daemon /kafka_2.11-1.1.0/config/zookeeper.properties
 
 # Start Druid
 
-/druid-0.11.0/bin/coordinator.sh start
-/druid-0.11.0/bin/overlord.sh start
-/druid-0.11.0/bin/middleManager.sh start
-/druid-0.11.0/bin/broker.sh start 
-/druid-0.11.0/bin/historical.sh start
+/druid-0.12.3/bin/coordinator.sh start
+/druid-0.12.3/bin/overlord.sh start
+/druid-0.12.3/bin/middleManager.sh start
+/druid-0.12.3/bin/broker.sh start 
+/druid-0.12.3/bin/historical.sh start
 
 # Start Kafka
 
-/kafka_2.11-1.0.0/bin/kafka-server-start.sh /kafka_2.11-1.0.0/config/server.properties
+/kafka_2.11-1.1.0/bin/kafka-server-start.sh /kafka_2.11-1.1.0/config/server.properties
